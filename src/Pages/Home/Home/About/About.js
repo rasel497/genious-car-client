572c5144860e23219cd02863f9f448009c673890
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-02 13:48:08 +0600

    Load services fake data and display service card dynamically

--- src/Pages/Home/Home/About/About.js
+++ src/Pages/Home/Home/About/About.js
@@ -4,13 +4,13 @@ import parts from '../../../../assets/images/about_us/parts.jpg';
 
 const About = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero my-20">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="relative w-1/2">
                     <img src={person} alt="" className="w-4/5 h-full rounded-lg shadow-2xl" />
                     <img src={parts} alt="" className="absolute w-3/5 top-1/2 border-8 right-5 rounded-lg shadow-2xl" /></div>
                 <div className='w-1/2'>
-                    <p className='text-2xl font-bold text-orange-600'>About Us</p>
+                    <p className='my-5 text-2xl font-bold text-orange-600'>About Us</p>
                     <h1 className="text-5xl font-bold">We are qualified <br />
                         & of experience <br />
                         in this field</h1>
