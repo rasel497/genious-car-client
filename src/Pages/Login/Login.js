ba8420dc38bde142dedd8bb5d791c2124340cd3a
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-05 13:39:57 +0600

    create JWT Token, Get jwt token on client side and store local storage

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -28,8 +28,28 @@ const Login = () => {
         signInUser(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
-                navigate(from, { replace: true });
+
+                const currentUser = {
+                    email: user.email
+                }
+                console.log(currentUser);
+
+                // get jwt token
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        // local storage is the easiest But not the best place to store jwt token
+                        localStorage.setItem('genius-token', data.token);
+                    })
+
+                // navigate(from, { replace: true });
                 form.reset();
             })
             .catch(err => console.log(err));
