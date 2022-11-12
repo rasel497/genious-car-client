a1711c68d471dce1c470220fa5f042fee503cd31
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-12 13:47:43 +0600

    shorting service order ascending to descending order With add bttn onClick auto change asc to desc

--- src/Pages/Home/Home/Services/Services.js
+++ src/Pages/Home/Home/Services/Services.js
@@ -3,13 +3,14 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [isAsc, setIsAsc] = useState(true);
 
     useEffect(() => {
-        fetch('https://genious-car-server-eosin.vercel.app/services')
+        fetch(`http://localhost:5000/services?order=${isAsc ? 'asc' : 'dsc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
 
-    }, [])
+    }, [isAsc]);
 
     return (
         <div>
@@ -17,6 +18,7 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className='text-5xl font-semibold'>Our Services Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+                <button className='btn btn-ghost bg-red-500' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'Reverse Desc' : 'Reverse Asc'}</button>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
 
